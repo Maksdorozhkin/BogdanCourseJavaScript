8b7e87da0cb53abb746681b2eb99497f240e0b4c
md <maksdorozhkin@gmail.com>
2023-08-01 21:40:15 +0300

    Объекты, динамическая типизация, переменные

---
+++ Variables.js
@@ -0,0 +1,22 @@
+let a;
+a = true;
+console.log(a);
+
+let myName;
+myName = "Maksim";
+console.log(myName);
+
+// присваивание переменных
+
+// Использование переменных в качестве значений свойств объекта
+const name = "Maks";
+const postQty = 23;
+
+const userProfile = {
+  name: name,
+  postQty: postQty,
+  hasSignedAgreement: false,
+};
+console.log(userProfile);
+
+//сокращенный вариант
---
+++ method.js
@@ -0,0 +1,15 @@
+// const myCity = {
+//     city: 'Dedowsk',
+//     cityGreeting: function () { // функция вместо свойства объекта - метод
+//         console.log('Greetings!!')
+//     }
+// }
+// myCity.cityGreeting() // вызов метода
+
+const myCity = {
+  myCity: "Dedowsk",
+  citiGreeting() {
+    console.log("Greeting!!!");
+  },
+};
+myCity.citiGreeting();
